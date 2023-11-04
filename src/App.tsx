5628c110f76586a85f245f9fbd984e915c6e44dd
ivanatrpkova <ivana.trpkova@codeflair.ch>
2023-11-04 14:20:38 +0100

    add Button component

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,15 @@
 import Input from "./components/Input.tsx";
+import Button from "./components/Button.tsx";
 
 function App() {
   return (
     <main>
       <Input id="name" label="Your name" type="text" />
-      <Input id="age" label="Your age" type="number" />
+      <p>
+        <Button el="anchor" href="www.google.con">
+          A link
+        </Button>
+      </p>
     </main>
   );
 }
