8269626507504c657a89e8f0594fc49f28ded6ff
ivanatrpkova <ivana.trpkova@codeflair.ch>
2023-11-05 20:48:41 +0100

    use forwardRef

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,13 @@
 import Input from "./components/Input.tsx";
 import Button from "./components/Button.tsx";
 import Container from "./components/Container.tsx";
+import { useRef } from "react";
 
 function App() {
+  const input = useRef<HTMLInputElement>(null);
   return (
     <main>
-      <Input id="name" label="Your name" type="text" />
+      <Input id="name" label="Your name" type="text" ref={input} />
       <p>
         <Button href="www.google.con">A link</Button>
       </p>
