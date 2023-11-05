a8106114fa9bbe64fd0c4910ef033237930f13a6
ivanatrpkova <ivana.trpkova@codeflair.ch>
2023-11-05 21:02:15 +0100

    add form component

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,16 @@
 import Input from "./components/Input.tsx";
 import Button from "./components/Button.tsx";
 import Container from "./components/Container.tsx";
+import Form from "./components/Form.tsx";
 import { useRef } from "react";
 
 function App() {
   const input = useRef<HTMLInputElement>(null);
+
+  function handleSave(data: unknown) {
+    const extractedData = data as { name: string; age: string };
+  }
+
   return (
     <main>
       <Input id="name" label="Your name" type="text" ref={input} />
@@ -14,6 +20,13 @@ function App() {
       <Container as={Button} onClick={() => alert("clicked")}>
         CLick me
       </Container>
+      <br />
+      <br />
+      <Form>
+        <Input type="text" label="name" id="name" />
+        <Input type="age" label="age" id="age" />
+        <Button>Save</Button>
+      </Form>
     </main>
   );
 }
