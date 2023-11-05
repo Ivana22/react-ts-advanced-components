4e0bd57ad13987398e216e4c2cca618e4b9935b7
ivanatrpkova <ivana.trpkova@codeflair.ch>
2023-11-05 11:41:16 +0100

    building a polymorphic component with generics

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,6 @@
 import Input from "./components/Input.tsx";
 import Button from "./components/Button.tsx";
+import Container from "./components/Container.tsx";
 
 function App() {
   return (
@@ -8,6 +9,9 @@ function App() {
       <p>
         <Button href="www.google.con">A link</Button>
       </p>
+      <Container as={Button} onClick={() => alert("clicked")}>
+        CLick me
+      </Container>
     </main>
   );
 }
